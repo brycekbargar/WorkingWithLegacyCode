5bee9ac9c1e75e52ced5d854647afd6fc7cc11a4
Bryce <brycekbargar@gmail.com>
2015-05-11 20:43:30 -0500

    Use our client

---
+++ Program.cs
@@ -0,0 +1,16 @@
+public namespace WorkingWithLegacyCode
+{
+	public class Program
+	{
+		static void Main() 
+        {
+            var container = new DIContainer().ThatGetsInitializedSomewhere();
+			
+			var service = container.Get<IBobsCrazyCycleService>();
+			
+			var sale = service.BuyACheapBike();
+			
+			Console.WriteLine(String.Format(@"Congrats! You bought the cheapest bike! Your reference number is {0}", sale.SaleNumber));
+        }
+	}
+}
